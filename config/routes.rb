f4e33b7e08a1bf7ed488667e4ec77bc99d354f55
clair13 <theclairvoyant1313@yahoo.gr>
2023-03-30 20:47:11 +0300

    Delete posts in realtime

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :discussions, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
-    resources :posts, only: [:create, :show, :edit, :update], module: :discussions
+    resources :posts, only: [:create, :show, :edit, :update, :destroy], module: :discussions
   end
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
