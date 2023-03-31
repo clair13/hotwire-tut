da4a0e2d0f0f9431fbd0d62d0cca40a2db078f5a
clair13 <theclairvoyant1313@yahoo.gr>
2023-03-31 19:53:03 +0300

    Add Categories scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_152600) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_163837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_152600) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "discussions_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "discussions", force: :cascade do |t|
     t.string "name"
     t.boolean "pinned", default: false
@@ -59,7 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_152600) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "posts_count"
+    t.integer "posts_count", default: 0
+    t.bigint "category_id"
     t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 
