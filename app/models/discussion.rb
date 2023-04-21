c6ddfa0db14b151baeada3fcc52579bd51d492e8
clair13 <theclairvoyant1313@yahoo.gr>
2023-04-21 18:26:05 +0300

    Update discussion model to have methods to toggle subscription status

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,10 +1,21 @@
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   belongs_to :category, counter_cache: true, touch: true
-  has_many :posts, dependent: :destroy
 
   # discussion.category_name
   delegate :name, prefix: :category, to: :category, allow_nil: true
+
+  validates :name, presence: true
+
+  has_many :posts, dependent: :destroy
+  has_many :users, through: :posts
+  has_many :discussion_subscriptions, dependent: :destroy
+  has_many :optin_subscribers, -> { where(discussion_subscriptions: { subscription_type: :optin }) },
+    through: :discussion_subscriptions,
+    source: :user
+  has_many :optout_subscribers, -> { where(discussion_subscriptions: { subscription_type: :optout }) },
+    through: :discussion_subscriptions,
+    source: :user
 
   accepts_nested_attributes_for :posts
 
@@ -12,13 +23,30 @@ class Discussion < ApplicationRecord
 
   broadcasts_to :category, inserts_by: :prepend
 
-  validates :name, presence: true
-
   after_create_commit -> { broadcast_prepend_to "discussions" }
   after_update_commit -> { broadcast_replace_to "discussions" }
   after_destroy_commit -> { broadcast_remove_to "discussions" }
 
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
+
+  def subscribed_users
+    (users + optin_subscribers).uniq - optout_subscribers
+  end
+
+  def subscription_for(user)
+    return nil if user.nil?
+    discussion_subscriptions.find_by(user_id: user.id)
+  end
+
+  def toggle_subscription(user)
+    if subscription = subscription_for(user)
+      subscription.toggle!
+    elsif posts.where(user_id: user.id).any?
+      discussion_subscriptions.create(user: user, subscription_type: "optout")
+    else
+      discussion_subscriptions.create(user: user, subscription_type: "optin")
+    end
   end
 end
